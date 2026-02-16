c8b9b570945a813edabcfd81aa71ddb463caefc5
douglassqueiroz18 <douglas.queiroz25@outlook.com>
2026-02-16 18:49:32 -0400

    ajustando home

---
+++ src/app/components/home/home.ts
@@ -0,0 +1,15 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  public irEditar(){
+    window.location.href = "/#/access";
+  }
+}
